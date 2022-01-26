cbbd66c5ab61a868d9a45b04ee8fbc04996f1bc2
Wendy Afrim <wendy.afrim2@gmail.com>
2022-01-26 16:30:11 +0100

    Récupération de la liste des articles
    - Configuration du fichier docker-compose
    - Variables d'environnement complétées dans le fichier config.php

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
 
-class Config {
-    public static $DB_HOST = '';
-    public static $DB_PORT = '';
-    public static $DB_USER = '';
-    public static $DB_PASSWORD = '';
-    public static $DB_NAME = '';
+class Config
+{
+    public static $DB_HOST = 'db';
+    public static $DB_PORT = '5432';
+    public static $DB_USER = 'root';
+    public static $DB_PASSWORD = 'root';
+    public static $DB_NAME = 'database';
 }
--- db.php
+++ db.php
@@ -2,13 +2,17 @@
 
 require_once './config.php';
 
-class DB {
+class DB
+{
     protected static $instance;
 
-    protected function __construct() {}
+    protected function __construct()
+    {
+    }
 
-    public static function getInstance() {
-        if(empty(self::$instance)) {
+    public static function getInstance()
+    {
+        if (empty(self::$instance)) {
             $db_info = array(
                 "db_host" => Config::$DB_HOST,
                 "db_port" => Config::$DB_PORT,
@@ -19,14 +23,12 @@ class DB {
 
             try {
                 self::$instance = new PDO("pgsql:host={$db_info['db_host']};port={$db_info['db_port']};dbname={$db_info['db_name']}", $db_info['db_user'], $db_info['db_pass']);
-                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);  
+                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
                 self::$instance->query('SET NAMES utf8');
                 self::$instance->query('SET CHARACTER SET utf8');
-
-            } catch(PDOException $error) {
+            } catch (PDOException $error) {
                 echo $error->getMessage();
             }
-
         }
 
         return self::$instance;
